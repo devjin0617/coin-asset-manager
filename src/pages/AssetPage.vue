2f9253140376b67776a960e17d149ee781fca18f
devjin0617 <devjin0617@gmail.com>
2017-10-06 23:06:37 +0900

    Add top menu

--- src/pages/AssetPage.vue
+++ src/pages/AssetPage.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <section>
-      <div style="height:80px; border-bottom:1px solid #b5b5b5; display:flex; align-items:center;">
-        <nav style="width:100%; max-width:768px; margin:auto;">
+      <div style="height:80px; display:flex; align-items:center;">
+        <nav style="width:100%; max-width:768px; margin:auto; padding:5px;">
           <a><span style="color:#333333; font-size:40px; font-weight:200;">CoinAM</span></a>
         </nav>
       </div>
     </section>
     <section>
-      <div style="width:100%; max-width:768px; background-color:blue; min-height:700px; margin:auto; margin-top:40px;">
-
+      <div style="width:100%; max-width:768px; min-height:700px; margin:auto;">
+        <div class="column" style="padding:0;">
+          <tabs :on-tab-click="tabMenu">
+            <tab-item label="AssetList"></tab-item>
+            <tab-item label="MarketCap"></tab-item>
+            <tab-item label="TalkList"></tab-item>
+            <tab-item label="More"></tab-item>
+          </tabs>
+        </div>
+        <div style="padding:5px;">
+          {{ pageName }}
+        </div>
       </div>
     </section>
   </div>
@@ -18,6 +28,11 @@
 <script>
 export default {
   name: 'asset-page',
+  data () {
+    return {
+      pageName: ''
+    }
+  },
   created () {
     window.document.getElementsByTagName('html')[0].className = 'main-view'
 
@@ -30,6 +45,11 @@ export default {
       // No user is signed in.
       console.log('login fail')
       this.$router.replace('/login')
+    }
+  },
+  methods: {
+    tabMenu (index) {
+      this.pageName = index
     }
   }
 }
