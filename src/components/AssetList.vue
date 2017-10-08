27d1ed91b27f5efee48e92a26596c4e64e7de364
devjin0617 <devjin0617@gmail.com>
2017-10-08 15:55:22 +0900

    Add sample coin data

--- src/components/AssetList.vue
+++ src/components/AssetList.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
-    AssetList View
+    <data-table :data="items" >
+      <column label="" field="icon"></column>
+      <column label="name" field="name"></column>
+      <column label="balance" field="balance"></column>
+      <column label="value" field="value">
+        <template scope="row">
+          <div>
+            {{ row.value.total }} | {{ row.value.current }}
+          </div>
+        </template>
+      </column>
+    </data-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'asset-list'
+  name: 'asset-list',
+  data () {
+    return {
+      items: [
+        {
+          key: 0,
+          icon: 'aa',
+          name: 'BTC',
+          balance: 2.45,
+          value: {
+            total: 12323500,
+            current: 5030000
+          }
+        }
+      ]
+    }
+  }
 }
 </script>
 
